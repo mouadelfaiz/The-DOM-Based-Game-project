2ac490b817e25fcdc593b6ecf83e2c95c41774e2
mouad <mouad_w8t68me@X1-Carbon.(none)>
2023-08-18 19:23:10 -0400

    set up the project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Rock Paper Scissors Game:
+## live site game:
+Git Hub live site: https://mouadelfaiz.github.io/The-DOM-Based-Game-project/
+## About The Game:
+This is a simple Rock Paper Scissors it takes two players the user (you) and the computer is The opponent. Both players have three choices rock paper or scissors the player can choose by clicking on the buttons and for the computer it shows a random choice.
+Before you choose there is a statement that tells you to make your choice and also it says the player who reach 10 point wins the Game.
+Every time you choose a computer also make a choice, after every choice there is a statement that says what was your choice and the computer’s choice and who wins the round, and under that you can see the point of both players.
+After one of the players reaches the 10 points the game is over and the statement will appear at the top to declare the winner of the game. After a second an alert is going to pop up and say the game is over and you should click restart to play again!!.
--- scripte.js
+++ scripte.js
@@ -1,11 +1,78 @@
-function whoWins(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-        return "It's a tie!";
-    } else if ((userChoice === "rock" && computerChoice === "scissors") ||
-               (userChoice === "paper" && computerChoice === "rock") ||
-               (userChoice === "scissors" && computerChoice === "paper")) {
-        return "You win!";
-    } else {
-        return "Computer wins!";
-    }
+let userScore = 0;
+let computerScore = 0;
+let gameOver = false;
+let winner;
+let result;
+
+const user = document.querySelector("#your-choice")
+const computer = document.querySelector("#computer-choice")
+const rock = document.querySelector("#Rock")
+const paper = document.querySelector("#Paper")
+const scissors = document.querySelector("#Scissors")
+const userScoreDisplay = document.querySelector("#user-score")
+const computerScoreDisplay = document.querySelector("#computer-score")
+const finalResult = document.querySelector(".result")
+const theWinner = document.querySelector(".winner")
+
+
+user.innerHTML = "👨"
+computer.innerHTML = "💻"
+
+function computerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"]
+  compChoice = choices[Math.floor(Math.random() * 3)];
+  if(compChoice === "Rock") {
+    computer.innerHTML = "&#9994"
+  }else if (compChoice === "Paper") {
+    computer.innerHTML = "&#9995"
+  }else {
+    computer.innerHTML = "&#9996"
+  }
+  return compChoice
 }
+
+function whoWins(uChoice, cChoice) {
+  if ((uChoice === "Rock" && cChoice === "Scissors") ||
+    (uChoice === "Paper" && cChoice === "Rock") ||
+    (uChoice === "Scissors" && cChoice === "Paper")) {
+    result = "You win the round!";
+    userScore++;
+  } else if (uChoice === cChoice) {
+    result = "It's a tie!";
+  }
+  else {
+    result = "Computer wins the round!";
+    computerScore++;
+  }
+if (userScore === 10 ) {
+  theWinner.innerHTML = "The winnner is the You"
+ 
+}else if (computerScore === 10) {
+  theWinner.innerHTML = "The winner is the Computer"
+  
+}
+ finalResult.innerText = `You chose: ${uChoice}, and The computer chose: ${cChoice}, ${result}`
+  userScoreDisplay.innerText = `Your Score: ${userScore} pt`
+computerScoreDisplay.innerText = `Computer Score: ${computerScore} pt`
+if (userScore === 10 || computerScore === 10) {
+ setTimeout(() => alert("The Game Is Over!!!! Click Restart"), 1000) 
+}
+  return result
+}
+
+
+rock.addEventListener('click', (event) => {
+    console.log(event) 
+    console.log(event.target.id)
+    user.innerHTML = "&#9994"
+  whoWins(event.target.id, computerChoice())
+})
+paper.addEventListener('click', (event) => {
+    user.innerHTML = "&#9995"
+  whoWins(event.target.id, computerChoice())
+})
+scissors.addEventListener('click', (event) => {
+    user.innerHTML = "&#9996"
+  whoWins(event.target.id, computerChoice())
+})
+
